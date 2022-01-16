0a805ae1f9d7e2c698ded07cad35336fb80cf63a
張翊飛 <YiFei.Chang@fei.idv.tw>
2022-01-16 12:06:07 +0800

    Add all files

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint[] mAnser = GetFibonacciNumbers(15);
+            Console.WriteLine(string.Join(", ", mAnser));
+        }
+        private static uint[] GetFibonacciNumbers(uint length)
+        {
+            uint[] mNumbers = new uint[length];
+            for (uint i = 0; i < mNumbers.Length; i++)
+            {
+                mNumbers[i] = GetFibonacciNumber(i + 1);
+            }
+            return mNumbers;
+        }
+        private static uint GetFibonacciNumber(uint n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return GetFibonacciNumber(n - 1) + GetFibonacciNumber(n - 2);
+            }
+        }
+    }
+}
